c4e7b99a02c27debe79c34c0fc6f8bd51d2c84b5
diegodelaFuenteCuraqueo <diego.delafuente@ug.uchile.cl>
2024-01-13 18:44:00 -0300

    fix class

--- src/views/course/index.js
+++ src/views/course/index.js
@@ -8,37 +8,8 @@ function Course({nombreClase = "nombre de la clase", videoSrc, descripcion= "des
     <div className="title-container">
       <h1 className="title text-start">Curso {nombreCurso}</h1>
     </div>
-    <div className="d-flex justify-content-center course-container" style={{borderColor: "green"}}>
-
-      <div className="home-element video-container">
-        <h2>{nombreClase}</h2>
-        <video width="900" height="600" controls src={videoSrc} ></video>
-        <br></br>
-        <div className="button-container">
-          <div className="button"><button className="btn btn-primary">anterior</button></div>
-          <div className="button"><button className="btn btn-primary">siguiente</button></div>
-        </div>
-      </div>
-
-      <div className=" home-element class-description">
-        <div className="class-description-element">
-          <h3>{nombreClase} - {nombreCurso}</h3>
-        </div>
-        <div className="class-description-element">
-          <p>{descripcion}</p>
-        </div>
-        <div className="class-description-element">
-          <h3>Cuestionario de la clase</h3>
-        </div>
-        <div className="class-description-element">
-          <p>Realiza el cuestionario para evaluar tus aprendizajes y continuar con el curso</p>
-        </div>
-        <div className="class-description-element">
-          <button className="btn btn-primary quiz-btn">Realizar Quiz</button>
-        </div>
-      </div>
+      <div className="d-flex justify-content-center course-container" style={{borderColor: "green"}}>
     </div>
-
     </>
   )
 }
