f932a9bd68fee7aa4d1e7f843b81c74fe3e075b9
Diego de la Fuente Curaqueo <diego.delafuente@ug.uchile.cl>
2024-01-18 18:54:23 -0300

    partial

--- src/views/malla/MallaDetail.js
+++ src/views/malla/MallaDetail.js
@@ -33,7 +33,7 @@ export default function MallaDetail() {
         <div className="text-center home-element">
           <div className="course-element-description">
             <p>
-            La ruta de aprendizaje que verás a continuación fue construida utilizando inteligencia artificial (IA) y se basa en tus intereses profesionales expresadas a través de nuestro formulario de registro.
+            La ruta de aprendizaje que verás a continuación fue construida utilizando inteligencia artificial (IA) y se basa en tus intereses profesionales expresados en el formulario de registro.
             </p>
             <p>
             Esta malla curricular personalizada no solo resalta cursos específicos, sino que también proporciona una guía detallada para tu travesía educativa. Cada curso recomendado se alinea con tus áreas de interés, ofreciéndote una formación más enfocada y relevante para tu vocación.
