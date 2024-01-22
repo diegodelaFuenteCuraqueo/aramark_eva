e36036386575495a0d9c9e973d0243cb100c3f8e
diegodelaFuenteCuraqueo <diego.delafuente@ug.uchile.cl>
2024-01-22 12:46:15 -0300

    partial

--- src/views/certificate/CertificateComponent.jsx
+++ src/views/certificate/CertificateComponent.jsx
@@ -5,7 +5,7 @@ export default function CertificateComponent() {
   return (
     <>
       <div className="title-container">
-        <h1 className="title text-start">Certificado</h1>
+        <h1 className="title text-start">¡Felicidades, has completado el curso!</h1>
       </div>
       <div className="diploma-container">
         <img src="/assets/diploma.png" alt="Certificate" className="diploma-image"></img>
